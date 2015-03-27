0e95d878b2b3416c825ef8a6bf70a997a98ff0fa
Mitzi Hazel Tizon <mitzi.bootyard@gmail.com>
2015-03-27 17:26:45 +0800

    Update

--- db/migrate/20150315040209_create_pins.rb
+++ db/migrate/20150315040209_create_pins.rb
@@ -1,8 +1,8 @@
 class CreatePins < ActiveRecord::Migration
-  def change
-    create_table :pins do |t|
-      t.string :description
+ def change
+   create_table :pins do |t|
+     t.string :description
 
-      t.timestamps
-    end
-end
+     t.timestamps
+   end
+ end
--- db/migrate/20150327071920_add_name_to_users.rb
+++ db/migrate/20150327071920_add_name_to_users.rb
@@ -1,5 +1,5 @@
 class AddNameToUsers < ActiveRecord::Migration
-  def change
+ def change
     add_column :users, :name, :string
   end
 end
